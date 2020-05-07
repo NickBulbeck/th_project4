4dc30b29637d02a5fbc49f54a0f60b075ebfbfb2
NickBulbeck <bulbecknick@gmail.com>
2020-05-07 20:58:53 +0100

    Added comments

--- js/Game.js
+++ js/Game.js
@@ -3,21 +3,27 @@
  * Game.js */
 
  class Game {
- 
-  static  messages = {
+ // The static "win" and "lose" properties should not be renamed as they correspond to 
+ // two CSS classes used on the overlay element in index.html.
+  static  gameOverMessage = {
       win: `Congratulations - you win! Click 'Start' to play again...`,
       lose: `Sorry - you lose! Click 'Start' to try again...`
-  } 
-
+  }
 
  	constructor(sourceData) {
- 		this.sourceData = sourceData;
+ 		this.sourceData = sourceData;        // array of key/value pairs
  		this.phrases = this.createPhrases();
  		this.missed = 0;
  		this.activePhrase = null;
  		this.usedPhrases = [];
  	}
-
+/* createPhrases method: called from the constructor; converts raw key/value pairs (from an
+                         external source) into Phrase objects. Key/value pair format is:
+                         text: string comprising only letters and spaces,
+                         level: string (in practice a single word)
+@params: none, although this.sourceData must be present in appropriate format.
+returns: an array of Phrase objects
+*/
  	createPhrases() {
  		const phraseObjects = [];
  		this.sourceData.forEach(sourcePhrase => {
@@ -27,6 +33,15 @@
  		return phraseObjects;
  	}
 
+/* getRandomPhrase method: picks a random Phrase object from this.phrases; removes it and pushes it
+                           into this.usedPhrases, to avoid the same Phrase being selected for
+                           two games in succession. If all Phrase objects have been used, recreates
+                           this.phrases using createPhrases and sets this.usedPhrases to an empty 
+                           array. Sets this.activePhrase to be the selected Phrase object.
+@params: none
+calls createPhrases, if there are no remaining Phrase objects in this.phrases
+returns: a single Phrase object
+*/
  	getRandomPhrase() {
     if (this.phrases.length === 0) {
         this.phrases = this.createPhrases();
@@ -40,22 +55,35 @@
  		return randomPhrase;
  	}
 
+/* startGame method: hides the overlay element, and calls addPhraseToDisplay on the Phrase
+                     object returned by getRandomPhrase.
+@params: none
+returns: null
+*/
  	startGame() {
  		const overlay = document.getElementById('overlay');
  		overlay.style.display = "none";
  		this.getRandomPhrase().addPhraseToDisplay()
  	}
 
- 	handleInteraction(letter) {
-    // console.log(this.activePhrase);
-    // console.log(this.activePhrase.hiddenLetters);
-    // console.log(this.activePhrase.chosenLetters);
+/* handleInteraction method: processes all user input. Note that user input may come from either
+                             a click event on a button, or a keydown event attached to the
+                             document at the top level. Thus, any reference to an event.target object
+                             would be ambiguous. So, I've avoided it in favour of selecting target
+                             element via DOM traversal.
+   @params: x, a single-character alphabetic string
+   calls:   activePhrase.checkLetter, activePhrase.showMatchedLetter, removeLife,
+            gameOver
+   returns: null
+*/
+ 	handleInteraction(x) {
+    // Button must be disabled following either a button-click OR a keystroke
     const buttonsCollection = document.getElementById('qwerty')
                            .getElementsByTagName('BUTTON');
     const button = Array.from(buttonsCollection)
-                        .filter(b => b.textContent === letter)[0];
- 		if (this.activePhrase.checkLetter(letter)) {
-      this.activePhrase.showMatchedLetter(letter);
+                        .filter(b => b.textContent === x)[0];
+ 		if (this.activePhrase.checkLetter(x)) {
+      this.activePhrase.showMatchedLetter(x);
       button.classList.add("chosen");
     } else {
       this.removeLife();
@@ -68,6 +96,10 @@
     }
  	}
 
+/*  checkForWin method: Detects a win or a loss
+    @params: none
+    returns: result, which is either "lose", "win" or null
+*/
   checkForWin() {
     let result = null;
     if (this.missed === 5) {
@@ -78,6 +110,11 @@
     return result;
   }
 
+/*  removeLife method: converts the leftmost "live" heart icon to a "lost" heart icon
+                       and increments this.missed.
+    @params: none
+    returns: null;
+*/
   removeLife() {
     const liveHearts = document.querySelectorAll(".tries");
     const heart = liveHearts[0];
@@ -86,6 +123,14 @@
     this.missed++;
   }
 
+/*  gameOver method: re-displays the overlay element with an appropriate win/lose message
+    calls:   clearTheBoard();
+    @params: result: a string with a value of "win" or "lose". This matches both the win and lose
+             static properties, and the .win and .lose classes in styles.css - so it's rather
+             brittle!  :-(
+             But hey.
+    returns: null;
+*/
   gameOver(result) {
     this.clearTheBoard();
     const overlay = document.getElementById('overlay');
@@ -93,9 +138,13 @@
     const finishingStyle = `start ${result}`
     overlay.classList = finishingStyle;
     const messageH1 = document.getElementById('game-over-message');
-    messageH1.textContent = Game.messages[result];
+    messageH1.textContent = Game.gameOverMessage[result];
   }
 
+/*  clearTheBoard method: resets the DOM to its original condition
+    @params: none;
+    returns: null;
+*/
   clearTheBoard() {
     this.missed = 0;
     const qwerty = document.getElementById('qwerty');
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,6 @@
+/* Treehouse FSJS Techdegree
+ * Project 4 - OOP Game App
+ * Phrase.js */
 
  class Phrase {
 
@@ -8,6 +11,11 @@
  		this.chosenLetters = '';
  	}
 
+/* addPhraseToDisplay method: creates an unordered list of letter elements matching the contents of
+                              this.text. Also adds an h3 element displaying this.level.
+@params: none
+returns: null
+*/
  	addPhraseToDisplay() {
     const displayDiv = document.getElementById('phrase');
  		const ul = displayDiv.getElementsByTagName('UL')[0];
@@ -31,9 +39,20 @@
  		displayDiv.insertBefore(levelHead,ul);
  	}
 
+/* checkLetter method: searches this.hiddenLetters and this.chosenLetters. Returns true if 
+                       the input parameter occurs in either, and false otherwise.
+                       A life will be removed if the method returns false; both strings are used because
+                       the character may have been input by clicking a button in the qwerty
+                       element, or by a keystroke. Whilst individual buttons in the qwerty 
+                       element can be disabled, the keystrokes can't. The method searches
+                       this.chosenLetters to avoid removing a life multiple times if the same
+                       key is pressed repeatedly.
+@params: x, a single alphabetic character
+returns: boolean
+*/
  	checkLetter(x) {
-    console.log(this.chosenLetters);
  		let found = false;
+    // arguably, could just search this.text!
  		if (this.hiddenLetters.includes(x) || this.chosenLetters.includes(x)) {
  			found = true;
  		}
@@ -41,7 +60,13 @@
  		return found;
  	}
 
-// Chosen a parameter of x because letter is already taken - too confusing
+/* showMatchedLetter method: called only when a letter matches one of the remaining letters in
+                             the target phrase. Displays all letter elements matching the input,
+                             and removes them from this.hiddenLetters so that future inputs can
+                             be tested against it.
+@params: x, a single alphabetic character
+returns: null
+*/
  	showMatchedLetter(x) {
  		let classes = ".hide.letter." + x;
  		const letters = document.querySelectorAll(classes);
